bf3e5fb5702085788671751761f1ad2a36c24c88
Paul Onawola <onawola.ire@gmail.com>
2023-01-29 17:37:31 +0100

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Paul Onawola on 29/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
